f3f1aab86285b944bff52bc55d45fc40366b546c
krytofin <kzn.dragons.craft@gmail.com>
2026-09-11 12:19:28 +0300

    feat: add diary app

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -44,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'diary',
 ]
 
 MIDDLEWARE = [
