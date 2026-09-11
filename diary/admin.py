e40d5e9a7d805f7c478b696d59df241bceb79e49
krytofin <kzn.dragons.craft@gmail.com>
2026-09-11 19:57:19 +0300

    feat(diary): configure admin for Homework and Subject models

--- diary/admin.py
+++ diary/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Homework, Subject
+
+
+admin.site.register(Subject)
+
+@admin.register(Homework)
+class HomeworkAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject__title', 'deadline', 'status', 'created_at', ]
+    list_filter = ['subject', 'deadline', 'status']
+    ordering = ['-deadline']
+    search_fields = ['title','description','subject__title']
+
--- diary/models.py
+++ diary/models.py
@@ -7,6 +7,9 @@ class Subject(models.Model):
     professor = models.CharField(max_length=255)
     is_completed = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f'{self.title}'
+
 
 class Homework(models.Model):
     subject = models.ForeignKey(to=Subject, on_delete=models.CASCADE, related_name='homeworks')
